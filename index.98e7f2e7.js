11eda0ca832452256a423ec704a48bc56951a6e7
Nikita Kalina <kalinanikita001@gmail.com>
2024-08-07 12:19:28 +0300

    make build

---
+++ index.98e7f2e7.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li");if(a.textContent=r,n.appendChild(a),Object.keys(t[r]>0)){var i=document.createElement("ul");a.appendChild(i),e(i,t[r])}}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.98e7f2e7.js.map
